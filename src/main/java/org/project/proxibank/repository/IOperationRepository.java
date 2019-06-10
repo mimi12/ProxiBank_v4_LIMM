c589860007f7d648b58b51839ce53d13cfa8da42
MarwaT30 <marwatifafi24@gmail.com>
2019-06-10 12:45:02 +0200

    test

--- src/main/java/org/project/proxibank/repository/IOperationRepository.java
+++
@@ -1,8 +0,0 @@
-package org.project.proxibank.repository;
-
-import org.project.proxibank.entities.Operation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IOperationRepository extends JpaRepository<Operation, Long> {
-
-}
